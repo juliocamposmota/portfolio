51d1c4ffd6ee52d5a2b3e379a4fa9ac88f461bed
julio-findme <julio@findme.id>
2024-10-14 19:03:33 -0300

    fix: adjust contact button

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import './styles/Home.css';
 
@@ -9,15 +8,13 @@ class Home extends Component {
       <div className="d-flex flex-column w-100 vh-100 mx-auto bg-hero">
         <Header />
 
-        <section className="px-3">
+        <section className="d-flex flex-column align-items-center gap-4">
           <h1>Júlio Mota Campos</h1>      
           <h2 className="lead">Desenvolvedor Web</h2>
 
-          <Link to="/">
-            <button type="button" className="btn btn-outline-light btn-lg rounded-pill">
-              START
-            </button>
-          </Link>
+          <a href="https://wa.me/55085999821177?text=Oi%20J%C3%BAlio.%20Quero%20desenvolver%20meu%20app!" className="btn btn-outline-light btn-lg rounded-pill" target="_blank" rel="noopener noreferrer">
+            CONTATO
+          </a>
         </section>
 
         <footer className="mt-auto mb-5 p-3">
