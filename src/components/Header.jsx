153fc76e0b1368921a8cd66c804c748b88475a26
julio-findme <julio@findme.id>
2024-10-15 06:24:18 -0300

    feat: add navbar icons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,25 @@
 import React, { Component } from 'react';
 
+import './styles/header.css';
+import { ReactComponent as GitHubIcon } from '../icons/github.svg';
+import { ReactComponent as BehanceIcon } from '../icons/behance.svg';
+import { ReactComponent as LinkedinIcon } from '../icons/linkedin.svg';
+
 class Header extends Component {
   render() {
     return (
       <header className="mb-auto">
         <nav className="navbar navbar-expand-md navbar-light bg-light p-3">
           <div className="container justify-content-center">
-            <ul className="navbar-nav">
+            <ul className="navbar-nav gap-3">
               <li className="nav-item">
-                <a className="nav-link" href="https://github.com/juliocamposmota" target="_blank" rel="noopener noreferrer">GitHub</a>
+                <a className="nav-link" href="https://github.com/juliocamposmota" target="_blank" rel="noopener noreferrer"><GitHubIcon /></a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="https://www.behance.net/juliomotacampos" target="_blank" rel="noopener noreferrer">Behance</a>
+                <a className="nav-link" href="https://www.behance.net/juliomotacampos" target="_blank" rel="noopener noreferrer"><BehanceIcon /></a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="https://www.linkedin.com/in/juliomotacampos" target="_blank" rel="noopener noreferrer">Linkedin</a>
+                <a className="nav-link" href="https://www.linkedin.com/in/juliomotacampos" target="_blank" rel="noopener noreferrer"><LinkedinIcon /></a>
               </li>
             </ul>
           </div>
