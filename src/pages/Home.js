d517b9e63f7b444b6275c00db495bb2a8a4d8680
julio-findme <julio@findme.id>
2024-10-15 10:41:40 -0300

    feat: add projects section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
-import Header from '../components/Header';
+
 import './styles/Home.css';
+import Header from '../components/Header';
+import projectTwo from '../images/app-archieoffice.png';
+import projectOne from '../images/website-gabrielledelima.png';
 
 class Home extends Component {
   render() {
     return (
-      <div className="d-flex flex-column w-100 vh-100 mx-auto bg-hero">
+      <div className="d-flex flex-column w-100 mx-auto bg-hero">
         <Header />
 
-        <section className="d-flex flex-column align-items-center gap-4">
-          <h1>Júlio Mota Campos</h1>      
+        <section className="d-flex flex-column justify-content-center align-items-center gap-4 vh-100">
+          <h1>Júlio Mota Campos</h1>
           <h2 className="lead">Desenvolvedor Web</h2>
 
           <a href="https://wa.me/55085999821177?text=Oi%20J%C3%BAlio.%20Quero%20desenvolver%20meu%20app!" className="btn btn-outline-light btn-lg rounded-pill" target="_blank" rel="noopener noreferrer">
@@ -17,7 +20,31 @@ class Home extends Component {
           </a>
         </section>
 
-        <footer className="mt-auto mb-5 p-3">
+        <section className="d-flex flex-column justify-content-center">
+          <h2 className='mb-5'>Projetos</h2>
+
+          <div className="d-flex flex-sm-column flex-lg-row justify-content-center align-items-center mt-5 gap-5 px-5">
+            <div className="card text-dark">
+              <img className="card-img-top img-size" src={projectOne} alt="Project Gabrielle de Lima Arquitetura" />
+
+              <div className="card-body">
+                <p class="card-text">Gabrielle de Lima Arquitetura e Interiores</p>
+                <a className="btn btn-outline-dark btn-sm rounded-3 w-100" href="https://gabrielledelima.arq.br/" target="_blank" rel="noopener noreferrer">VISITE</a>
+              </div>
+            </div>
+
+            <div className="card text-dark">
+              <img className="card-img-top img-size" src={projectTwo} alt="Project Archie Office" />
+
+              <div className="card-body">
+                <p class="card-text">Archie Office</p>
+                <a className="btn btn-outline-dark btn-sm rounded-3 w-100" href="https://archieoffice.netlify.app/login" target="_blank" rel="noopener noreferrer">VISITE</a>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <footer className="mt-5 p-3">
           <p>
             Feito com muito carinho por mim mesmo :)
             <a href="https://www.instagram.com/juliomotac/" className="text-white ms-1" target="_blank" rel="noopener noreferrer">@juliomotac</a>
